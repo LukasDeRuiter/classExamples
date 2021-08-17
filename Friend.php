79e5666b78c32d0d0dedd193c67d1ff794f8aa80
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-17 21:30:45 +0200

    added getters and setters

--- Friend.php
+++ Friend.php
@@ -1,13 +1,35 @@
 <?php
 class Friend {
 
-    public $name;
-    public $age;
+    //properties
+
+    private $name;
+    private $age;
     
+    //constructors
+
     public function __construct($name, $age) {
         $this->name =  $name;
         $this->age = $age;
 
+    }
+
+    //Getters and Setters
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
     }
 }
 
